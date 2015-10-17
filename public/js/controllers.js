8ac1044e0cd7cb03bcd09e1e22febbfa250dac2d
Henrry Chuquimia <henrry.el.master@gmail.com>
2015-10-17 08:07:51 -0400

    clean...

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,13 +1,5 @@
 angular.module("FinalApp")
 .controller("MainController", function($scope){
-	$scope.opendDialog = function(dialogId)
-	{
-	    LxDialogService.open(dialogId);
-	};
-
-	$scope.closingDialog = function()
-	{
-	    LxNotificationService.info('Dialog closed!');
-	};
+	
 
 });
