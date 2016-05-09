9b8191f64661ff3314512f87788cc99fbb70824e
21daniela10 <211092danielaaa@gmail.com>
2016-05-10 01:48:31 +0200

    adicione xxxx

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // set up ======================================================================
+
 var express  = require('express');
 var app      = express(); 								// create our app w/ express
 //var mongoose = require('mongoose'); 					// mongoose for mongodb
@@ -25,3 +26,5 @@ require('./app/routes/home.js')(app);
 // listen (start app with node server.js) ======================================
 app.listen(port);
 console.log("App listening on port " + port);
+
+//xxxx
